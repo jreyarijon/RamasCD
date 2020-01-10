00033f50312c9d845301e3235b0f401cd444b82e
unknown <jreyarijon@danielcastelao.org>
2020-01-10 14:00:10 +0100

    segundo commit

--- src/ramascd/RamasCD.java
+++ src/ramascd/RamasCD.java
@@ -17,6 +17,7 @@ public class RamasCD {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("HOLA K ASEEEE");
+        //Comentario generico jajajajajaj
     }
     
 }
